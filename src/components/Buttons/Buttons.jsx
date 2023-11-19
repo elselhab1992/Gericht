bbad2342ed94072d31f02159a703248321a20b9f
Mohamed Elselhab <momanchester1992@gmail.com>
2023-11-19 15:16:46 +0000

    add styles to the btn

---
+++ src/components/Buttons/Buttons.jsx
@@ -0,0 +1,8 @@
+/* eslint-disable react/prop-types */
+import "./Buttons.css";
+
+const Buttons = ({ children }) => {
+  return <button className="btn">{children}</button>;
+};
+
+export default Buttons;
